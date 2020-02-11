98d762f8fb925a6a9047db00c0aa4aaf65d27335
eees4n <eees4ns@gmail.com>
2020-02-11 18:44:42 +0900

    two files

--- git_test.py
+++ git_test.py
@@ -2,3 +2,4 @@
 print("Hello World!")
 print("git add でステージングエリアに追加")
 print("git commit でローカルリポジトリにコミット")
+print("git rm でステージングエリアから削除")
---
+++ git_test_second.py
@@ -0,0 +1,2 @@
+# -*- coding: shift_jis -*-
+print("2‚Â–Ú‚Ìƒtƒ@ƒCƒ‹")
